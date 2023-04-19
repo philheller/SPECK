cb222b56da150ce802d84b4a57075020f81ad1e1
uqpyx <80069620+hichemBA67@users.noreply.github.com>
2023-04-19 14:23:50 +0200

    Change next_id to Counters.Counter

--- app/contracts/Speck.sol
+++ app/contracts/Speck.sol
@@ -2,9 +2,11 @@
 pragma solidity >=0.8.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Speck is ERC721 {
-    uint256 private next_id;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     struct Product {
         string id;
@@ -23,20 +25,32 @@ contract Speck is ERC721 {
     }
 
     mapping(uint256 => Product) private _products;
+    mapping(uint256 => address) private _tokenOwner;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-        next_id = 1;
-    }
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {}
 
-    function createNewProduct(Product memory _product_data) public {
-        _safeMint(msg.sender, next_id);
-        _products[next_id] = _product_data;
-        next_id++;
+    function createNewProduct(
+        Product memory _product_data
+    ) public returns (uint256) {
+        //Incremented tokenId for starting the tokenId from 1.
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+        _products[newItemId] = _product_data;
+        return newItemId;
     }
 
     function getProductData(
         uint256 _token_id
     ) public view returns (Product memory) {
         return _products[_token_id];
+    }
+
+    function totalAmout() public view returns (uint256) {
+        return _tokenIds.current();
     }
 }
