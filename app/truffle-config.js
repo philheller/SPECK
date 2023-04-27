951f5745d8fff3aed14086387989029d8c86f87d
uqpyx <80069620+hichemBA67@users.noreply.github.com>
2023-04-27 21:00:22 +0200

    Change gasLimit

--- app/truffle-config.js
+++ app/truffle-config.js
@@ -50,6 +50,7 @@ module.exports = {
       host: "ganache-cli", // For use with all services in Docker
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
+      gas: 9700000,
     },
     // Another network with more advanced options...
     // advanced: {
