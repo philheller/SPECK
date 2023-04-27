4e5861568282102ca2edb69825abfe8ca009d990
uqpyx <80069620+hichemBA67@users.noreply.github.com>
2023-04-27 21:53:15 +0200

    Add checks for createNewProduct

--- app/contracts/Speck.sol
+++ app/contracts/Speck.sol
@@ -48,19 +48,18 @@ contract Speck is ERC721 {
     function createNewProduct(
         Product memory _product_data
     ) public onlyRegistered {
-        // TODO: UNCOMMENT PREVIOUS PRODUCT CHECK AND OWNERSHIP CHECK
-        // if (_product_data.previous_product != 0) {
-        //     uint256 previousProductId = _product_data.previous_product;
-        // //     Check if previous product exists
-        //     require(
-        //         bytes(_products[previousProductId].id).length > 0,
-        //         "SPECK: Previous product does not exist"
-        //     );
-        //     require(
-        //         ownerOf(_product_data.previous_product) == msg.sender,
-        //         "SPECK: You are not the owner of the previous product."
-        //     );
-        // }
+        if (_product_data.previous_product != 0) {
+            uint256 previousProductId = _product_data.previous_product;
+            //     Check if previous product exists
+            require(
+                bytes(_products[previousProductId].id).length > 0,
+                "SPECK: Previous product does not exist."
+            );
+            require(
+                ownerOf(_product_data.previous_product) == msg.sender,
+                "SPECK: You are not the owner of the previous product."
+            );
+        }
 
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
